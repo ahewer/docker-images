3abdcd5d3bab3435854acc746bb073b0d0c7814c
Alexander Hewer <hewer@coli.uni-saarland.de>
2020-04-10 12:11:31 +0200

    setup locale

--- runtime-image/setup.sh
+++ runtime-image/setup.sh
@@ -3,15 +3,15 @@ apt-get -y update && \
 	openjdk-8-jre-headless \
 	python \
 	blender \
-	r-base \
-	r-recommended \
 	git \
 	libann0 \
 	libarmadillo8 \
 	libjsoncpp1 \
 	libcairomm-1.0-1v5 \
 	libinsighttoolkit4.12 \
-	libyaml-cpp0.5v5 && \
+	libyaml-cpp0.5v5 \
+	locales && \
 	apt-get -y autoremove && \
 	apt-get clean && \
-	rm -rf /var/lib/apt/lists/*
+	rm -rf /var/lib/apt/lists/* && \
+	localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
